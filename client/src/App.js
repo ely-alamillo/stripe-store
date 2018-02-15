9f1fa6c4d7539af02b0e785c140c3e366d0e0b1b
Ely Alamillo <ely0013@gmail.com>
2018-02-14 18:50:50 -0600

    added stripe checkout button

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import StripeCheckout from 'react-stripe-checkout';
 import './App.css';
 
 class App extends Component {
+  onToken = token => {
+    console.log('token: ', token);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <StripeCheckout
+          token={this.onToken}
+          stripeKey="pk_test_lQChvBC3SslCU83lTaDxPst5"
+        />
       </div>
     );
   }
