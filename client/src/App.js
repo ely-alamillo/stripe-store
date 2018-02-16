ddfe00db90ea63b1c6053b11d24ff3ad4f47a3f9
Ely Alamillo <ely0013@gmail.com>
2018-02-15 22:51:58 -0600

    added grommet components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
-import Button from 'grommet/components/Button';
-import './App.css';
 
-class App extends Component {
+import App from 'grommet/components/App';
+import Title from 'grommet/components/Title';
+
+import StripeCheckout from 'react-stripe-checkout';
+import Button from 'grommet/components/Button';
+import axios from 'axios';
+// import './App.scss';
+
+class Home extends Component {
   onToken = token => {
     axios
-      .post('http://localhost:3001/api/checkout', { token: token.id })
+      .post('http://localhost:3021/api/checkout', { token: token.id })
       .then(res => {
         console.log('res: ', res);
       })
@@ -18,7 +22,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <App className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
@@ -31,9 +35,17 @@ class App extends Component {
           stripeKey="pk_test_lQChvBC3SslCU83lTaDxPst5"
         />
         <Button label="Label" href="#" />
-      </div>
+      </App>
     );
   }
 }
 
-export default App;
+// export default () => (
+//   <App>
+//     <Title>Hello World</Title>
+//     <p>Hello from a Grommet page!</p>
+//     <Button label="Label" href="#" />
+//   </App>
+// );
+
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+// import './index.scss';
+import 'grommet/scss/vanilla/index.scss';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const element = document.getElementById('root');
+ReactDOM.render(<App />, element);
