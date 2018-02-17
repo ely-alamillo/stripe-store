88e0f6916e96729c50dbe1942d428523a9cc28df
Ely Alamillo <ely0013@gmail.com>
2018-02-16 19:44:36 -0600

    changed component to be shell

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
-
+import { Route, withRouter } from 'react-router-dom';
+import Home from './components/home/Home';
 import App from 'grommet/components/App';
-import Title from 'grommet/components/Title';
 
-import StripeCheckout from 'react-stripe-checkout';
-import Button from 'grommet/components/Button';
-import axios from 'axios';
-// import './App.scss';
-
-class Home extends Component {
-  onToken = token => {
-    axios
-      .post('http://localhost:3021/api/checkout', { token: token.id })
-      .then(res => {
-        console.log('res: ', res);
-      })
-      .catch(err => {
-        console.log('ther was an error', err);
-      });
-  };
-
+class Shell extends Component {
   render() {
     return (
       <App className="App">
@@ -27,25 +11,10 @@ class Home extends Component {
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <StripeCheckout
-          token={this.onToken}
-          stripeKey="pk_test_lQChvBC3SslCU83lTaDxPst5"
-        />
-        <Button label="Label" href="#" />
+        <Route path="/" exact component={Home} />
       </App>
     );
   }
 }
 
-// export default () => (
-//   <App>
-//     <Title>Hello World</Title>
-//     <p>Hello from a Grommet page!</p>
-//     <Button label="Label" href="#" />
-//   </App>
-// );
-
-export default Home;
+export default Shell;
