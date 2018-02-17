a296378caacba57d22b526c4017a489a74da3e6a
Ely Alamillo <ely0013@gmail.com>
2018-02-16 19:44:52 -0600

    moved app to be home component

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import App from 'grommet/components/App';
+import Title from 'grommet/components/Title';
+
+import StripeCheckout from 'react-stripe-checkout';
+import Button from 'grommet/components/Button';
+import axios from 'axios';
+// import './App.scss';
+
+class Home extends Component {
+  onToken = token => {
+    axios
+      .post('http://localhost:3021/api/checkout', { token: token.id })
+      .then(res => {
+        console.log('res: ', res);
+      })
+      .catch(err => {
+        console.log('ther was an error', err);
+      });
+  };
+
+  render() {
+    return (
+      <App className="App">
+        <p className="App-intro">Home page yo</p>
+        <StripeCheckout
+          token={this.onToken}
+          stripeKey="pk_test_lQChvBC3SslCU83lTaDxPst5"
+        />
+        <Button label="Label" href="#" />
+      </App>
+    );
+  }
+}
+
+export default Home;
