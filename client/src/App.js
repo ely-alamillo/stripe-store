6d6ca6f7fc591757aeaf85d9538ac5189183e658
Ely Alamillo <ely0013@gmail.com>
2018-02-17 16:11:55 -0600

    fixed nav not showing issue

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import Navgation from './components/navigation/Navigation';
 import Home from './components/home/Home';
 import Store from './components/store/store';
 import App from 'grommet/components/App';
+import Box from 'grommet/components/Box';
 import Navigation from './components/navigation/Navigation';
 
 class Shell extends Component {
   render() {
     return (
-      <div>
-        <Navigation />
-        {/* <Switch>
-          <Route path="/store" component={Store} />
-          <Route path="/" component={Home} />
-        </Switch> */}
-      </div>
+      <Box>
+        <App centered={false}>
+          <Navigation />
+          <Switch>
+            <Route path="/store" component={Store} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </App>
+      </Box>
     );
   }
 }
