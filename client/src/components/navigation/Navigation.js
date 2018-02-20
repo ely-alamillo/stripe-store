d69666d07678aa20c6a5f00c8f18a744114ed461
Ely Alamillo <ely0013@gmail.com>
2018-02-20 16:08:12 -0600

    added padding on header

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -6,28 +6,39 @@ import Search from 'grommet/components/Search';
 import Menu from 'grommet/components/Menu';
 import Anchor from 'grommet/components/Anchor';
 import Actions from 'grommet/components/icons/base/Actions';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+import App from 'grommet/components/App';
 
 const Navigation = props => {
   return (
-    <Header>
-      <Title>Stripe Shop</Title>
-      <Box flex={true} justify="end" direction="row" responsive={false}>
-        <Search
+    <Box pad="medium">
+      <Header>
+        <Title>Stripe Shop</Title>
+        <Box flex={true} justify="end" direction="row" responsive={false}>
+          {/* <Search
           inline={true}
           fill={true}
           size="medium"
           placeHolder="Search"
           dropAlign={{ right: 'right' }}
-        />
-        <Menu icon={<Actions />} dropAlign={{ right: 'right' }}>
-          <Anchor href="#" className="active">
-            First
-          </Anchor>
-          <Anchor href="#">Second</Anchor>
-          <Anchor href="#">Third</Anchor>
-        </Menu>
-      </Box>
-    </Header>
+        /> */}
+          <Menu
+            icon={<Actions />}
+            dropAlign={{ right: 'right' }}
+            inline={true}
+            direction="row"
+          >
+            <LinkContainer to={'/'}>
+              <Anchor>Home</Anchor>
+            </LinkContainer>
+            <LinkContainer to={'/store'}>
+              <Anchor>Store</Anchor>
+            </LinkContainer>
+          </Menu>
+        </Box>
+      </Header>
+    </Box>
   );
 };
 export default Navigation;
