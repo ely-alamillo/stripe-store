7349eaeb6a6256b0167dad7ea658a2d6af7aa335
Ely Alamillo <ely0013@gmail.com>
2018-02-20 19:36:11 -0600

    added some styles

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -25,28 +25,33 @@ class Home extends Component {
             backgroundColorIndex="dark"
           >
             <Box direction="row" justify="center" align="center">
-              <Box basis="1/2" align="end" pad="medium">
-                <Heading margin="none">Sample Heading One</Heading>
+              <Box
+                align="end"
+                pad="medium"
+                style={{ backgroundColor: 'rgba(100, 100, 100, 0.8)' }}
+              >
+                <Heading margin="none">Welcome!</Heading>
               </Box>
-              <Box basis="1/2" align="start" pad="medium">
-                <Heading margin="none">Sample Heading Two</Heading>
-              </Box>
+              {/* <Box basis="1/2" align="start" pad="medium">
+                <Heading margin="none">Welcome!</Heading>
+              </Box> */}
             </Box>
           </Hero>
           <Section>
             <Box direction="row" justify="center" align="center">
-              <Box align="center" pad="medium">
-                <Heading margin="none">Sample Heading Two</Heading>
-                <Paragraph>
-                  Raised on hip-hop and foster care, defiant city kid Ricky gets
-                  a fresh start in the New Zealand countryside. He quickly finds
-                  himself at home with his new foster family: the loving Aunt
-                  Bella, the cantankerous Uncle Hec, and dog Tupac. When a
-                  tragedy strikes that threatens to ship Ricky to another home,
-                  both he and Hec go on the run in the bush. As a national
-                  manhunt ensues, the newly branded outlaws must face their
-                  options: go out in a blaze of glory or overcome their
-                  differences and survive as a family.
+              <Box align="center" pad="small" basis="1/2">
+                <Heading margin="none">Who We Are</Heading>
+              </Box>
+              <Box align="center" pad="small" basis="1/2">
+                <Heading margin="none">Stripe Shop</Heading>
+                <Paragraph size="large" align="start">
+                  Built from the ground up, Stripe Shop is a humble online store
+                  aiming to please all of our customers. We pride ourselves in
+                  honest bussiness and outstanding customer service. We are
+                  always looking to provide the best products and services to
+                  all of our customers. If there is anything we can do better
+                  don't hesitate to contact us and we will do our best to meet
+                  your needs.
                 </Paragraph>
               </Box>
             </Box>
